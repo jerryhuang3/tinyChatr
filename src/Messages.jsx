d6dc691fc5a1834d4db400ea6e02b54f9b0e44fd
jerryhuang3 <jerryhuang@utexas.edu>
2019-04-09 02:00:31 +0000

    passed hardcoded props from MessageList to Messages

--- src/Messages.jsx
+++ src/Messages.jsx
@@ -1,62 +1,16 @@
 import React, { Component } from 'react';
 
 class Messages extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      messages: [
-        {
-          type: 'incomingMessage',
-          content:
-            "I won't be impressed with technology until I can download food.",
-          username: 'Anonymous1'
-        },
-        {
-          type: 'incomingNotification',
-          content: 'Anonymous1 changed their name to nomnom'
-        },
-        {
-          type: 'incomingMessage',
-          content:
-            "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
-          username: 'Anonymous2'
-        },
-        {
-          type: 'incomingMessage',
-          content: '...',
-          username: 'nomnom'
-        },
-        {
-          type: 'incomingMessage',
-          content:
-            "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
-          username: 'Anonymous2'
-        },
-        {
-          type: 'incomingMessage',
-          content: "This isn't funny. You're not funny",
-          username: 'nomnom'
-        },
-        {
-          type: 'incomingNotification',
-          content: 'Anonymous2 changed their name to NotFunny'
-        }
-      ]
-    };
-  }
+  
   render() {
-    const messageContent = this.state.messages.map(message => {
-      return message.username ? (
-        <div className="message">
-          <span className="message-username">{message.username}</span>
-          <span className="message-content">{message.content}</span>
-        </div>
-      ) : (
-        <div className="message system">{message.content}</div>
-      );
-    });
-
-    return <div className="messages">{messageContent}</div>;
+    return this.props.username ? (
+          <div className="message">
+            <span className="message-username">{this.props.username}</span>
+            <span className="message-content">{this.props.content}</span>
+          </div>
+        ) : (
+          <div className="message system">{this.props.content}</div>
+        );
   }
 }
 export default Messages;
